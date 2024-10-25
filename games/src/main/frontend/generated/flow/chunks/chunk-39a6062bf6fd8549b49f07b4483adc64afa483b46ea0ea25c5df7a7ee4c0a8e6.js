2b7f9309967b08819bb6eab80a8e3c5d36f24854
Mariosor12 <marioavena120898@gmail.com>
2024-10-25 14:40:36 -0400

    commit

---
+++ games/src/main/frontend/generated/flow/chunks/chunk-39a6062bf6fd8549b49f07b4483adc64afa483b46ea0ea25c5df7a7ee4c0a8e6.js
@@ -0,0 +1,11 @@
+import '@vaadin/tooltip/theme/lumo/vaadin-tooltip.js';
+import '@vaadin/polymer-legacy-adapter/style-modules.js';
+import '@vaadin/button/theme/lumo/vaadin-button.js';
+import 'Frontend/generated/jar-resources/buttonFunctions.js';
+import '@vaadin/dialog/theme/lumo/vaadin-dialog.js';
+import 'Frontend/generated/jar-resources/flow-component-renderer.js';
+import '@vaadin/vertical-layout/theme/lumo/vaadin-vertical-layout.js';
+import '@vaadin/horizontal-layout/theme/lumo/vaadin-horizontal-layout.js';
+import '@vaadin/icon/theme/lumo/vaadin-icon.js';
+import '@vaadin/icons/vaadin-iconset.js';
+import '@vaadin/notification/theme/lumo/vaadin-notification.js';
---
+++ games/src/main/frontend/generated/flow/chunks/chunk-5bd30f8b09a2281006e43d00b76c60d97c5f854c6e5117bd24ee69170924d579.js
@@ -0,0 +1,3 @@
+import '@vaadin/polymer-legacy-adapter/style-modules.js';
+import '@vaadin/vertical-layout/theme/lumo/vaadin-vertical-layout.js';
+import '@vaadin/horizontal-layout/theme/lumo/vaadin-horizontal-layout.js';
